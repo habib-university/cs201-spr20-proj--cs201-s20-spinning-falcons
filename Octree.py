e260458d839870028a7a57de392404ec2195193f
hmmmmmmenergy <54717435+hmmmmmmenergy@users.noreply.github.com>
2020-06-03 19:11:48 +0500

    Update Octree.py

--- Octree.py
+++ Octree.py
@@ -1,6 +1,8 @@
 class Octree:
-    def __init__(self):
-        self.Root_Node = Node(0, 0, 0)
+    def __init__(self, x, y, z, x1, y1, z1):
+        self.Top_Right = Point(x, y, z)
+        self.Back_Left = Point(x1, x1, z1)
+        self.Root_Node = Node((x+x1)/2, (y+y1)/2, (z+z1)/2)
         self.Height = 0
         self.Max_Size = 0  # size to not be exceeed
         self.Total_Nodes = 1
@@ -18,6 +20,14 @@ class Octree:
 
     def Add(self, Node):
         pass
+
+
+class Point:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
 
 
 class Node:
